907ac588ca165d7cffc003453930b79bb192c1bd
Nouran Hisham <hishamnouran4@gmail.com>
2026-01-05 09:58:39 +0200

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# AWS / Localstack SQS
+AWS_REGION = "ap-south-1"
+AWS_ENDPOINT_URL = "http://localstack:4566"
+AWS_ACCESS_KEY = "test"
+AWS_SECRET_KEY = "test"
+
+SQS_MAX_MESSAGES = 10
+SQS_WAIT_TIME = 5
+
+# Database (PostgreSQL)
+DB_HOST = "postgres"
+DB_PORT = 5432
+DB_NAME = "events_db"
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+
+# App Settings
+POLLING_SLEEP_SECONDS = 5
